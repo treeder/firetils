3fd05b2246a9d271f78d72475141a9eb3f081ef9
Travis Reeder <treeder@gmail.com>
2019-10-28 13:08:35 -0400

    Initial commit

---
+++ firetils.go
@@ -0,0 +1,43 @@
+package firetils
+
+import (
+	"time"
+	"cloud.google.com/go/firestore"
+)
+
+type Firestored struct {
+	Ref *firestore.DocumentRef `firestore:"-" json:"-"`
+}
+
+type TimestampedI interface {
+	GetCreatedAt() time.Time
+	GetUpdatedAt() time.Time
+	SetCreatedAt(time.Time)
+	SetUpdatedAt(time.Time)
+}
+type Timestamped struct {
+	UpdatedAt time.Time `firestore:"updated_at" json:"updated_at"`
+	CreatedAt time.Time `firestore:"created_at" json:"created_at"`
+}
+
+func (ts *Timestamped) GetCreatedAt() time.Time {
+	return ts.CreatedAt
+}
+func (ts *Timestamped) GetUpdatedAt() time.Time {
+	return ts.UpdatedAt
+}
+func (ts *Timestamped) SetCreatedAt(t time.Time) {
+	ts.CreatedAt = t
+}
+func (ts *Timestamped) SetUpdatedAt(t time.Time) {
+	ts.UpdatedAt = t
+}
+
+// UpdateTimeStamps call this right before storing it in a database
+func UpdateTimeStamps(obj TimestampedI) TimestampedI {
+	if obj.GetCreatedAt().IsZero() {
+		obj.SetCreatedAt(time.Now())
+	}
+	obj.SetUpdatedAt(time.Now())
+	return obj
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/treeder/firetils
+
+go 1.13
+
+require cloud.google.com/go/firestore v1.0.0
