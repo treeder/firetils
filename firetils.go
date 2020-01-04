7ff114eedb4a2f557c67f9bcaf0a63a9eb406a54
Travis Reeder <treeder@gmail.com>
2020-01-04 18:06:25 -0500

    wip

--- firetils.go
+++ firetils.go
@@ -1,43 +1,66 @@
 package firetils
 
 import (
-	"time"
+	"context"
+	"errors"
+
 	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go"
+	"github.com/treeder/gotils"
+	"go.uber.org/zap"
+	"google.golang.org/api/option"
 )
 
-type Firestored struct {
-	Ref *firestore.DocumentRef `firestore:"-" json:"-"`
-}
+// var (
+// 	app      *firebase.App // todo: move this somewhere more general
+// 	fireauth *auth.Client
+// 	client   *firestore.Client
+// )
 
-type TimestampedI interface {
-	GetCreatedAt() time.Time
-	GetUpdatedAt() time.Time
-	SetCreatedAt(time.Time)
-	SetUpdatedAt(time.Time)
-}
-type Timestamped struct {
-	UpdatedAt time.Time `firestore:"updated_at" json:"updated_at"`
-	CreatedAt time.Time `firestore:"created_at" json:"created_at"`
-}
-
-func (ts *Timestamped) GetCreatedAt() time.Time {
-	return ts.CreatedAt
-}
-func (ts *Timestamped) GetUpdatedAt() time.Time {
-	return ts.UpdatedAt
-}
-func (ts *Timestamped) SetCreatedAt(t time.Time) {
-	ts.CreatedAt = t
-}
-func (ts *Timestamped) SetUpdatedAt(t time.Time) {
-	ts.UpdatedAt = t
-}
-
-// UpdateTimeStamps call this right before storing it in a database
-func UpdateTimeStamps(obj TimestampedI) TimestampedI {
-	if obj.GetCreatedAt().IsZero() {
-		obj.SetCreatedAt(time.Now())
+// New creates a new firestore client
+// Call defer client.Close() after this if you can
+func New(ctx context.Context, projectID string, opts []option.ClientOption) (*firestore.Client, error) {
+	// Use the application default credentials
+	var err error
+	conf := &firebase.Config{ProjectID: projectID}
+	app, err := firebase.NewApp(ctx, conf, opts...)
+	if err != nil {
+		gotils.L(ctx).Sugar().Fatalf("couldn't init firebase newapp: %v\n", err)
+		return nil, err
 	}
-	obj.SetUpdatedAt(time.Now())
-	return obj
+	// fireauth, err := app.Auth(ctx)
+	// if err != nil {
+	// 	gotils.L(ctx).Sugar().Fatalf("error getting Auth client: %v\n", err)
+	// 	return nil, err
+	// }
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		gotils.L(ctx).Sugar().Fatalf("couldn't init firestore: %v\n", err)
+		return nil, err
+	}
+	return client, nil
+}
+
+// func FirebaseApp() *firebase.App {
+// 	return app
+// }
+
+// func FirebaseAuth() *auth.Client {
+// 	return fireauth
+// }
+
+// func DefaultClient() *firestore.Client {
+// 	return client
+// }
+
+func SaveGeneric(ctx context.Context, collection, id string, ow *Timestamped) (*firestore.DocumentRef, *Timestamped, error) {
+	UpdateTimeStamps(ow)
+	ref := client.Collection(collection).Doc(id)
+	_, err := ref.Set(ctx, ow)
+	if err != nil {
+		gotils.L(ctx).Error("Failed to save generic object!", zap.Error(err))
+		return nil, nil, errors.New("Failed to store object, please try again")
+	}
+	return ref, ow, nil
 }
